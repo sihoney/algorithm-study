81bd322e610d1a00117dc1b4b90624d613565cc2
황희원 <82200359+sihoney@users.noreply.github.com>
2026-01-18 21:43:36 +0900

    phase2 - string, phase3 - math

--- src/baekjoon/phase3/math/PrimeCount_1978.java
+++ src/baekjoon/phase3/math/PrimeCount_1978.java
@@ -1,4 +1,42 @@
 package baekjoon.phase3.math;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 소수 찾기
+// https://www.acmicpc.net/problem/1978
 public class PrimeCount_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+
+        while(st.hasMoreTokens()) {
+            if(isPrime(Integer.parseInt(st.nextToken()))) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    static boolean isPrime(int v) {
+        if(v == 1) return false;
+
+        for(int i = 2; i < v; i++) {
+            if(v % i == 0) return false;
+        }
+
+        return true;
+    }
 }
+
+/**
+ * prime
+ * 1보다 큰 자연수 중 1과 자기 자신만을 약수로 갖는 수
+ * 2, 3, 5, 7 등
+ */
--- src/baekjoon/phase3/math/PrimeRange_2581.java
+++ src/baekjoon/phase3/math/PrimeRange_2581.java
@@ -1,4 +1,44 @@
 package baekjoon.phase3.math;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 소수
+// https://www.acmicpc.net/problem/2581
 public class PrimeRange_2581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int minPrime = Integer.MAX_VALUE;
+
+        for(int i = m; i <= n; i++) {
+            if(isPrime(i)) {
+                sum += i;
+                if(i < minPrime) minPrime = i;
+            }
+        }
+
+        if(sum == 0) {
+            System.out.println(-1);
+        }
+        else {
+            System.out.println(sum);
+            System.out.println(minPrime);
+        }
+    }
+
+    static boolean isPrime(int v) {
+        if(v == 1) return false;
+
+        for(int i = 2; i < v; i++) {
+            if(v % i == 0) return false;
+        }
+
+        return true;
+    }
 }
