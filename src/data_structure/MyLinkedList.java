ccdf5f97118e9e36bea8764efbca7dd27d3b0a43
황희원 <82200359+sihoney@users.noreply.github.com>
2026-01-27 23:45:25 +0900

    data_structure | LinkedList

--- src/data_structure/MyLinkedList.java
+++ src/data_structure/MyLinkedList.java
@@ -25,7 +25,7 @@ class MyLinkedList {
     public void addFirst(int x) {
         // TODO
         Node newNode = new Node(x);
-        newNode.next = head; // Q. next에 접근 어떻게 해?
+        newNode.next = head;
 
         head = newNode;
         if(size == 0) tail = newNode;
@@ -38,7 +38,8 @@ class MyLinkedList {
         Node newNode = new Node(x);
         tail.next = newNode;
 
-        Node tail = newNode;
+        tail = newNode;
+        if(size == 0) head = newNode;
 
         size++;
     }
@@ -47,29 +48,42 @@ class MyLinkedList {
         // TODO: 없으면 NoSuchElementException
         if(size == 0) throw new NoSuchElementException();
 
-        Node nextNode = head.next;
+        Node currNode = head;
 
-        head = nextNode;
+        head = head.next;
 
         size--;
 
-        return -1;
+        return currNode.val;
     }
 
     public int removeLast() {
         // TODO: 없으면 NoSuchElementException
         if(size == 0) throw new NoSuchElementException();
 
+        Node tailRemoved = tail;
 
+        Node node = head;
+        for(int i = 1; i < size - 1; i++) {
+            node = node.next;
+        }
+        tail = node;
 
-        return -1;
+        size--;
+
+        return tailRemoved.val;
     }
 
     public int get(int index) {
         // TODO: 범위 밖이면 IndexOutOfBoundsException
+        if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
 
+        Node target = head;
+        for(int idx = 0; idx < index; idx++) {
+            target = target.next;
+        }
 
-        return -1;
+        return target.val;
     }
 
     public int size() {
